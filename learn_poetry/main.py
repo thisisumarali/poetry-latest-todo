5a49ea91b34fd8d082fc4f5c2b3fa268e3001ab3
Umar Ali <umeralikhan7864@gmail.com>
2024-04-07 16:59:44 +0500

    updating

--- learn_poetry/main.py
+++ learn_poetry/main.py
@@ -1,47 +1,99 @@
-from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-from typing import Optional
-from dotenv import dotenv_values
 
-class Task(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+
+class HeroBase(SQLModel):
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
 
-# Load environment variables from .env file
-env_vars = dotenv_values(".env")
-sqlite_url = env_vars.get("SQLITE_URL")
 
-# Create SQLAlchemy engine
-engine = create_engine(sqlite_url)
+class Hero(HeroBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    hashed_password: str = Field()
 
-# Function to create database tables
+
+class HeroCreate(HeroBase):
+    password: str
+
+
+class HeroRead(HeroBase):
+    id: int
+
+
+class HeroUpdate(SQLModel):
+    name: str | None = None
+    secret_name: str | None = None
+    age: int | None = None
+    password: str | None = None
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Load the ML model
+
+def hash_password(password: str) -> str:
+    # Use something like passlib here
+    return f"not really hashed {password} hehehe"
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
     create_db_and_tables()
-    yield
 
-# Create FastAPI app instance
-app: FastAPI = FastAPI(lifespan=lifespan)
 
-# Route to create a new task
-@app.post("/task/")
-def create_task(task: Task):
+@app.post("/heroes/", response_model=HeroRead)
+def create_hero(hero: HeroCreate):
+    hashed_password = hash_password(hero.password)
     with Session(engine) as session:
-        session.add(task)
+        extra_data = {"hashed_password": hashed_password}
+        db_hero = Hero.model_validate(hero, update=extra_data)
+        session.add(db_hero)
         session.commit()
-        session.refresh(task)
-        return task
+        session.refresh(db_hero)
+        return db_hero
 
-# Route to retrieve all task
-@app.get("/task/")
-def read_task():
+
+@app.get("/heroes/", response_model=list[HeroRead])
+def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
     with Session(engine) as session:
-        task = session.exec(select(Task)).all()
-        return task
+        heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
+        return heroes
+
+
+@app.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return hero
+
+
+@app.patch("/heroes/{hero_id}", response_model=HeroRead)
+def update_hero(hero_id: int, hero: HeroUpdate):
+    with Session(engine) as session:
+        db_hero = session.get(Hero, hero_id)
+        if not db_hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        hero_data = hero.model_dump(exclude_unset=True)
+        extra_data = {}
+        if "password" in hero_data:
+            password = hero_data["password"]
+            hashed_password = hash_password(password)
+            extra_data["hashed_password"] = hashed_password
+        db_hero.sqlmodel_update(hero_data, update=extra_data)
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+        return db_hero
